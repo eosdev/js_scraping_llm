71f1d57dda5c83f053b114a0473ff65ceafcfe32
eosdev <eden.oliveira405@gmail.com>
2023-11-24 23:44:27 -0300

    FFEAT: ajusta cria a fun;áo de prompt

--- browser/config.js
+++ browser/config.js
@@ -1,6 +1,6 @@
 //config.js
 
-const PROXY_URL = "http://br.smartproxy.com:10000";
+const PROXY_URL = "http://br.teste:10000";
 const PROXY_USERNAME = "user";
 const PROXY_PASSWORD = "password";
 
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const cheerio = require("cheerio");
 const instancePlaywright = require('./browser/instancePlaywright');
+const {Configuration, OpenAIApi} = require('openai');
+
+require('dotenv').config();
+
+//chamada da API
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
 
 
 async function getHTML(url) {
+    //Funcao que vai realizar a leitura do HTML
 
     const instance  = await instancePlaywright(url, false);
     
@@ -13,5 +23,20 @@ async function getHTML(url) {
 };
 
 
+async function getPrompt(html) {
+    const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "system", content: "Say this is a test" }, //orienta comportamento do seu modelo durante toda a conversa
+            { role: "user", content: html }
+        ],
+        
+    });
+}
 
-getHTML('http://htmlpreview.github.io/?https://github.com/twbs/bootstrap/blob/gh-pages/2.3.2/index.html')
+
+
+//getHTML('http://htmlpreview.github.io/?https://github.com/twbs/bootstrap/blob/gh-pages/2.3.2/index.html')
+
+//usar variavel de ambiente
+//console.log(process.env.MY_ENV_VAR);
